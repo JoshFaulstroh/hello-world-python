7b658da7d392fe7d8f8136051e3a36107725653e
JoshFaulstroh <joshfaulstroh@gmail.com>
2023-11-06 05:36:25 -0600

    DONE

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,8 @@
+import time
 
+print("\nHello World!\n")
+time.sleep(1)
+print("\nHow are you today?\n")
+time.sleep(1)
+print("\nThis was printed using the Python programming language!\n")
+time.sleep(1)
